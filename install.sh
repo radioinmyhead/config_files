14698b567984f1d942de7f3bca7bd334cd505ae2
zxt <zxt@megvii.com>
2017-09-13 14:15:03 +0800

    add darwin

--- install.sh
+++ install.sh
@@ -1,21 +1,58 @@
 #!/bin/bash
-type git || exit
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd `dirname $0`
-function install(){
+
+# install config file
+install(){
   local file=$HOME/.$1
   ls $file &>/dev/null && rm -rf $file
   echo $file
   ln -s `pwd`/$1 $file
 }
 
+install_vim(){
+  type git || return 1
+  git clone --branch v0.10.2 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+  #vim +GoInstallBinaries
+}
+
+# install for Darwin
+darwin(){
+  local files="bash_profile bash-powerline.sh vimrc gitconfig"
+  for f in $files; do
+    install $f
+  done
+  
+  install_vim
+}
+
+linux(){
+type git || exit
+cd `dirname $0`
+
 files="bashrc vimrc gitconfig tmux.conf"
 for f in $files; do
   install $f
 done
 
-vim +PluginInstall +qall
-go get github.com/constabulary/gb/...
-go get github.com/tools/godep
-go get -u github.com/nsf/gocode
+}
 
+
+get_os(){
+  if uname -a | grep -iq darwin;then
+    echo darwin && return 0
+  fi
+  if uname -a | grep -iq linux;then
+    echo linux && return 0
+  fi
+}
+
+os=$(get_os)
+case $os in
+  darwin)
+    darwin
+    ;;
+  linux)
+    ;;
+  *)
+    echo fail
+esac
